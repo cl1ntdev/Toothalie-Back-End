7ead406715f40ad47f4668afd859e5ae56bae2b2
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-01 14:42:51 +0800

    sample dentist

--- src/Controller/API/Authenticated/GetDentists.php
+++ src/Controller/API/Authenticated/GetDentists.php
@@ -13,24 +13,41 @@ class GetDentists extends AbstractController {
     {
         // Sample data (later you can fetch from DB)
         $dentists = [
+        [
             [
                 "name" => "Dr. Jane Doe",
                 "email" => "jane.doe@toothalie.com",
                 "experience" => "8 years",
-                "specialty" => "Orthodontist"
+                "specialty" => "Orthodontist",
+                "schedule" => [
+                    "Monday" => ["09:00AM", "11:00AM", "01:00PM"],
+                    "Wednesday" => ["10:00AM", "02:00PM"],
+                    "Friday" => ["09:30AM", "01:30PM", "03:00PM"]
+                ]
             ],
             [
                 "name" => "Dr. John Smith",
                 "email" => "john.smith@toothalie.com",
                 "experience" => "12 years",
-                "specialty" => "Endodontist"
+                "specialty" => "Endodontist",
+                "schedule" => [
+                    "Tuesday" => ["09:00AM", "11:30AM", "03:30PM"],
+                    "Thursday" => ["10:00AM", "12:00PM", "04:00PM"],
+                    "Saturday" => ["08:00AM", "10:00AM"]
+                ]
             ],
             [
                 "name" => "Dr. Maria Garcia",
                 "email" => "maria.garcia@toothalie.com",
                 "experience" => "6 years",
-                "specialty" => "Pediatric Dentist"
+                "specialty" => "Pediatric Dentist",
+                "schedule" => [
+                    "Monday" => ["10:00AM", "01:00PM", "03:00PM"],
+                    "Thursday" => ["09:30AM", "11:30AM", "01:30PM"],
+                    "Friday" => ["08:00AM", "12:00PM"]
+                ]
             ]
+        ]
         ];
 
         return new JsonResponse([
