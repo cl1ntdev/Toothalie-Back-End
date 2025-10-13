6a370117f48f097ceb1e3b2ce4b310d529a7e2d8
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-13 17:20:54 +0800

    inital for updating appointment status by dentist

---
+++ src/Controller/API/Authenticated/DentistAppointment/UpdateAppointment.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\API\Authenticated\DentistAppointment;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\DBAL\Connection;
+
+class UpdateAppointment extends AbstractController
+{
+    #[Route('/api/edit-appointment-dentist', name: "edit-appointment-dentist", methods: ['POST'])]
+    public function getAppointment(Request $req, Connection $connection): JsonResponse
+    {
+        try {
+            $data = json_decode($req->getContent(), true);
+            $appointmentID = $data['appointment_id'] ?? null;
+            $status = $data['status'];
+            
+            
+            if (!$appointmentID) {
+                return new JsonResponse([
+                    'status' => 'error',
+                    'message' => 'Missing appointmentid'
+                ], 400);
+            }
+
+            $update = $connection->update(
+                "appointment",
+                ['status' => $status],
+                ['appointment_id' => $appointmentID]
+            );
+            
+
+          
+            return new JsonResponse([
+                'status' => 'ok',
+                'update_status' => $update 
+            ]);
+        } catch (\Exception $e) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+}
