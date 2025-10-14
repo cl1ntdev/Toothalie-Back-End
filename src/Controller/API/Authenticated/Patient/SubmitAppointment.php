25061f1209eb2bbbd39d5fa2e91c42c126f604ff
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-10-14 23:31:17 +0800

    renamed for clarity

--- src/Controller/API/Authenticated/Patient/SubmitAppointment.php
+++ src/Controller/API/Authenticated/Patient/SubmitAppointment.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\API\Authenticated\PatientAppointment;
+namespace App\Controller\API\Authenticated\Patient;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\DBAL\Connection;
 
-class Submit extends AbstractController
+class SubmitAppointment extends AbstractController
 {
     // >> >> >> << << << 
     // 
--- src/Controller/API/Authenticated/Patient/UpdateAppointment.php
+++ src/Controller/API/Authenticated/Patient/UpdateAppointment.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\API\Authenticated\PatientAppointment;
+namespace App\Controller\API\Authenticated\Patient;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
