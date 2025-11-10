bb829c80789ed28675ac3ece48dc746d763dc4e1
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-11-11 00:03:08 +0800

    activity 2 final

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,20 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-
-        return new Response("Welcome to dashboard! (Protected Area)");
-        // return $this->json([
-        //     'message' => 'Welcome to your new controller!',
-        //     'path' => 'src/Controller/DashboardController.php',
-        // ]);
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
     }
 }
